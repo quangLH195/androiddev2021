d86a6ea21b3061c90be825496be3ad082379ed54
quangLH195 <59214933+quangLH195@users.noreply.github.com>
2021-01-04 11:30:21 +0700

    upload till practical work 11
    I'm sorry for being late because some bugs and some error in the app

--- USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
+++ USTHWeather/app/src/main/java/vn/edu/usth/weather/WeatherActivity.java
@@ -1,55 +1,86 @@
 package vn.edu.usth.weather;
 
+import androidx.annotation.NonNull;
+import androidx.annotation.Nullable;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.FragmentManager;
+import androidx.viewpager.widget.ViewPager;
 
 import android.os.Bundle;
 import android.util.Log;
+import android.widget.TextView;
+
+import com.google.android.material.tabs.TabLayout;
 
 public class WeatherActivity extends AppCompatActivity {
 
-    private static final String TAG = "WeatherActivity";
+    WeatherAndForecastFragment weatherAndForecastFragment;
+    ViewPager viewPager;
+    TextView pagination;
+    int total = 4, count = 1;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(@Nullable Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_weather);
 
-        Log.i(TAG, "onCreate: ");
+        viewPager = findViewById(R.id.view_pager);
+        pagination = findViewById(R.id.pagination);
+        TabLayout tabLayout = findViewById(R.id.tabLayout);
+        tabLayout.setupWithViewPager(viewPager);
 
-        FragmentManager fragmentManager = getSupportFragmentManager();
-        ForecastFragment firstFragment = new ForecastFragment();
-        getSupportFragmentManager().beginTransaction().add(R.id.container, firstFragment).commit();
+        viewPager.setAdapter(new SliderAdapter(getSupportFragmentManager()));
+
+        pagination.setText(count + "/" + total);
+
+        viewPager.addOnPageChangeListener(new ViewPager.OnPageChangeListener() {
+            @Override
+            public void onPageScrolled(int position, float positionOffset, int positionOffsetPixels) {
+
+            }
+
+            @Override
+            public void onPageSelected(int position) {
+                count = 0;
+                count = position + 1;
+                pagination.setText(count + "/" + total);
+            }
+
+            @Override
+            public void onPageScrollStateChanged(int state) {
+
+            }
+        });
     }
 
     @Override
     protected void onStart() {
         super.onStart();
-        Log.i(TAG, "onStart: ");
+        Log.i(getClass().getName(), "Weather Activity onStart");
     }
 
     @Override
     protected void onResume() {
         super.onResume();
-        Log.i(TAG, "onResume: ");
+        Log.i(getClass().getName(), "Weather Activity onResume");
     }
 
     @Override
     protected void onPause() {
         super.onPause();
-        Log.i(TAG, "onPause: ");
+        Log.i(getClass().getName(), "Weather Activity onPause");
     }
 
     @Override
     protected void onStop() {
         super.onStop();
-        Log.i(TAG, "onStop: ");
+        Log.i(getClass().getName(), "Weather Activity onStop");
     }
 
     @Override
     protected void onDestroy() {
         super.onDestroy();
-        Log.i(TAG, "onDestroy: ");
+        Log.i(getClass().getName(), "Weather Activity onDestroy");
     }
 
 }
